613b84cf3d441e3826c29381cc37d30d79ea0f3b
manish <gowardipemanish95@gmail.com>
2022-03-04 18:29:19 +0530

    add bin

---
+++ bin/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ROOT="$(
+    cd "$(dirname "$0")" >/dev/null 2>&1 || exit
+    pwd -P
+)"
+cd $ROOT/../
+
+echo "Starting component-ui app"
+
+if [ -d "node-modules" ]
+then
+    npm start
+else
+    npm install && npm start
+fi
