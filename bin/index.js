194abbe4305fff74362a7489e50473a81a47edb5
manish <gowardipemanish95@gmail.com>
2022-03-04 18:55:08 +0530

    try 9

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/no-nodejs-modules
 import { exec } from 'child_process'
-
+console.log('helloooo')
 const myShellScript = exec('sh ./bin/start.sh')
 myShellScript.stdout.on('data', (data) => {
   console.log(data)
