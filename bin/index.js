a155cca2657b35f69af8a176c9d6993a30dc9f45
manish <gowardipemanish95@gmail.com>
2022-03-04 19:01:57 +0530

    try 12

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable import/no-nodejs-modules */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const exec = require('child_process')
+import exec from 'child_process'
 console.log('helloooo')
 const myShellScript = exec('sh ./bin/start.sh')
 myShellScript.stdout.on('data', (data) => {
